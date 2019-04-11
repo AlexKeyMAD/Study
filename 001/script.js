abdf4380b9454371257154ad441cffac8b9a11e6
Alexandr <it_np@mail.ru>
2019-04-11 19:35:09 +0600

    Первая запись

---
+++ 001/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let t;
+
+console.log('hello');
+console.log(t);
+
+let obj = {
+    mame: "Alex",
+    age: 36,
+    man: true
+};
+
+console.log(obj.age);
+
+//alert('Hello');
+
+confirm('Ho-ho');
---
+++ 001/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let obj = {
+    mame: "Alex",
+    age: 36,
+    man: true
+};
+
+console.log(obj.mame)
