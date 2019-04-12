9fc76c546e04af279f86e3c7746f1b40a9f8b968
Alexandr <it_np@mail.ru>
2019-04-12 12:20:35 +0600

    Третья запись

--- 002/Script.js
+++ 002/Script.js
@@ -1,10 +1,25 @@
 'use strict';
 
-var money = console.count('Ваш бюджет на месяц?');
+var money = prompt('Ваш бюджет на месяц?',"");
 
-var time = console.count('Введите дату в формате ГГГГ-ММ-ДД');
+var time = prompt('Введите дату в формате ГГГГ-ММ-ДД',"");
 
-var appData 
-{
-    Budjet = money;
+var appData = {
+    Budjet: money,
+    timeData: time,
+    expenses: {
+        ans1: 
+        [prompt('Обязательная статья расхода №1',""),
+        prompt('Цена вопроса?',"")],
+        ans2: 
+        [prompt('Обязательная статья расхода №2',""),
+        prompt('Цена вопроса?',"")]
+    },
+    optionalExpenses: {},
+    income: [],
+    saving: false
 }
+
+var BudjetDay = appData.Budjet / 30;
+
+alert(BudjetDay);
