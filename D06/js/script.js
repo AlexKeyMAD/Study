ebb1987c6990d394da402927f97ff803cca2d2ad
Alexandr <it_np@mail.ru>
2019-04-16 17:41:36 +0600

    12-я запись

---
+++ D06/js/script.js
@@ -0,0 +1,25 @@
+let btStart = document.getElementById('start');
+
+let budget = document.getElementsByClassName('budget-value')[0],
+    daybudget = document.getElementsByClassName('daybudget-value')[0],
+    level = document.getElementsByClassName('level-value')[0],
+    expenses = document.getElementsByClassName('expenses-value')[0],
+    buoptionalexpensesdget = document.getElementsByClassName('optionalexpenses-value')[0],
+    income = document.getElementsByClassName('income-value')[0],
+    monthsavings = document.getElementsByClassName('monthsavings-value')[0],
+    yearsavings = document.getElementsByClassName('yearsavings-value')[0];
+
+let expenses_item = document.getElementsByClassName('expenses-item');
+
+let buttons = document.getElementsByTagName('button');
+
+let optionalexpenses_item = document.querySelectorAll('optionalexpenses-item');
+
+let choose_income = document.querySelector('.choose-income'),
+    checksavings = document.querySelector('.checksavings'),
+    choose_sum = document.querySelector('.choose-sum'),
+    choose_percent = document.querySelector('.choose-percent'),
+    checksavings = document.querySelector('.checksavings'),
+    year = document.querySelector('.year'),
+    month = document.querySelector('.month'),
+    day = document.querySelector('.day');
