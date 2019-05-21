cc5832e3bd17297582ad0a814cfac786d7e30605
Alexandr <it_np@mail.ru>
2019-05-21 13:54:42 +0600

    s031

--- D09/script.js
+++ D09/script.js
@@ -154,4 +154,53 @@ window.addEventListener('DOMContentLoaded',function()
     let div_ = new Options(10,10,10,"20px",'ttttttt');
     div_.NewTextDiv();
 
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо за обращение! Мы скоро с вами свяжемся',
+        failure: 'Ошибка обработки'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function() {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+
+        request.open('POST', 'server.php');
+        //request.setRequestHeader('Content-Type', 'application/x-www-from-urlencoded');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        //request.send(formData);
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState == 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+
+    });
+
 });
